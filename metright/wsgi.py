5f3f8bb62d6f6097886ed7d27858768cdb16a4ea
Olusola I.T <50151968+Tolushawlar@users.noreply.github.com>
2023-12-22 22:11:03 +0000

    update to render

--- metright/wsgi.py
+++ metright/wsgi.py
@@ -1,14 +1,14 @@
 
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metright.settings')
-
-# application = get_wsgi_application()
 import os
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'metright.settings')
+
 application = get_wsgi_application()
-app = application
+# import os
+# from django.core.wsgi import get_wsgi_application
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+# application = get_wsgi_application()
+# app = application
